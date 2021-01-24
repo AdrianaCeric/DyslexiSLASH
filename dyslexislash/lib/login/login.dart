334c093f46224d61f6ac3369a98b666169863458
Akanksha1212 <akankshasingh121297@gmail.com>
2021-01-24 21:30:34 +0530

    Azure auth fixed

--- dyslexislash/lib/login/login.dart
+++ dyslexislash/lib/login/login.dart
@@ -1,6 +1,9 @@
 import 'package:dyslexislash/initial_screen.dart';
 import 'package:dyslexislash/screens/homepage.dart';
+import 'package:aad_oauth/aad_oauth.dart';
+import 'package:aad_oauth/model/config.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -8,8 +11,20 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  static final Config config = Config(
+    tenant: 'f5e8d153-117b-4e4a-b90c-444a852abf97',
+    clientId: 'd7464976-8865-49fc-a5c8-7c4ee51b033f',
+    scope: 'openid profile offline_access',
+    redirectUri: 'https://login.live.com/oauth20_desktop.srf',
+  );
+  final AadOAuth oauth = AadOAuth(config);
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    var rectSize =
+        Rect.fromLTWH(0.0, 25.0, screenSize.width, screenSize.height - 25);
+    oauth.setWebViewScreenSize(rectSize);
+
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       backgroundColor: Color(0xff7ccccc),
@@ -18,101 +33,96 @@ class _LoginState extends State<Login> {
         children: [
           Padding(
             padding: EdgeInsets.only(left: 30, right: 30),
-            child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/Wave.png',
-                  ),
-                  Container(
-                    color: Color(0xffD8EFEF),
-                    child: new TextFormField(
-                      decoration: new InputDecoration(
-                        prefixIcon:
-                            Icon(Icons.mail_outline, color: Color(0xff5CA6A6)),
-                        labelText: "Enter your Email",
-                        fillColor: Color(0xffD8EFEF),
-                      ),
-                      validator: (val) {
-                        if (val.length == 0) {
-                          return "Email cannot be empty";
-                        } else {
-                          return null;
-                        }
-                      },
+            child: Center(
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/Wave.png',
                     ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    color: Color(0xffD8EFEF),
-                    child: new TextFormField(
-                      decoration: new InputDecoration(
-                        prefixIcon:
-                            Icon(Icons.lock_open, color: Color(0xff5CA6A6)),
-                        labelText: "Enter your Password",
-                        fillColor: Color(0xffD8EFEF),
-                      ),
-                      validator: (val) {
-                        if (val.length == 0) {
-                          return "Password cannot be empty";
-                        } else {
-                          return null;
-                        }
-                      },
+                    Container(
+                      height: 58,
+                      width: MediaQuery.of(context).size.width / 1.5,
+                      child: RaisedButton(
+                          color: Color(0xffD8EFEF),
+                          child: Text(
+                            'Sign In',
+                            style: GoogleFonts.poppins(
+                              textStyle: TextStyle(
+                                color: Color(0xff5CA6A6),
+                                fontSize: 25,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                          onPressed: () {
+                            login();
+                          }),
                     ),
-                  ),
-                  SizedBox(
-                    height: 50,
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.check_circle,
-                    ),
-                    iconSize: 100,
-                    color: Color(0xffD8EFEF),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => InitialScreen(),
-                        ),
-                      );
-                    },
-                  ),
-                  // Container(
-                  //   height: 45,
-                  //   width: MediaQuery.of(context).size.width,
-                  //   child: RaisedButton(
-                  //     child: Text(
-                  //       'LOG IN',
-                  //       style:
-                  //           TextStyle(color: Color(0xff6202ee), fontSize: 15),
-                  //     ),
-                  //     color: Colors.white,
-                  //     shape: RoundedRectangleBorder(
-                  //         borderRadius: BorderRadius.circular(28),
-                  //         side: BorderSide(color: Color(0xff6202ee))),
-                  //     onPressed: () {
-                  //       // LOGIN write code here
-                  //       //  Navigator.push(
-                  //       //     context,
-                  //       //     MaterialPageRoute(
-                  //       //       builder: (context) => LogIn(),
-                  //       //     ),
-                  //       //   );
-                  //     },
-                  //   ),
-                  // ),
-                ],
+                    // Container(
+                    //   height: 45,
+                    //   width: MediaQuery.of(context).size.width,
+                    //   child: RaisedButton(
+                    //     child: Text(
+                    //       'LOG IN',
+                    //       style:
+                    //           TextStyle(color: Color(0xff6202ee), fontSize: 15),
+                    //     ),
+                    //     color: Colors.white,
+                    //     shape: RoundedRectangleBorder(
+                    //         borderRadius: BorderRadius.circular(28),
+                    //         side: BorderSide(color: Color(0xff6202ee))),
+                    //     onPressed: () {
+                    //       // LOGIN write code here
+                    //       //  Navigator.push(
+                    //       //     context,
+                    //       //     MaterialPageRoute(
+                    //       //       builder: (context) => LogIn(),
+                    //       //     ),
+                    //       //   );
+                    //     },
+                    //   ),
+                    // ),
+                  ],
+                ),
               ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  void showError(dynamic ex) {
+    showMessage(ex.toString());
+  }
+
+  void showMessage(String text) {
+    var alert = AlertDialog(content: Text(text), actions: <Widget>[
+      FlatButton(
+          child: const Text('Ok'),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => InitialScreen()));
+          })
+    ]);
+    showDialog(context: context, builder: (BuildContext context) => alert);
+  }
+
+  void login() async {
+    try {
+      await oauth.login();
+      var accessToken = await oauth.getAccessToken();
+      showMessage('Logged in successfully');
+    } catch (e) {
+      showError(e);
+    }
+  }
+
+  void logout() async {
+    await oauth.logout();
+    showMessage('Logged out');
   }
 }
