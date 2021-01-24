08b06cfe7b4961e11f62a23ae53f6a5f39ca7ce6
Akanksha1212 <akankshasingh121297@gmail.com>
2021-01-24 10:41:04 +0530

    Added splash screen

---
+++ dyslexislash/android/app/src/main/kotlin/com/example/dyslexislash/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dyslexislash
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dyslexislash/lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+
+import 'package:dyslexislash/screens/homepage.dart';
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 4), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    });
+  }
+
+  // added test yourself
+  // and made the text to align at center
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff7ccccc),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Image.asset(
+                'assets/Logo.png',
+                height: 250,
+                width: 250,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
